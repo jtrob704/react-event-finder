90e81e2f43111ea11a7cb3259e6bf8676e32df70
jtrob704 <jaittarius@hotmail.com>
2020-02-16 17:27:08 -0500

    Initial commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './Header.css'
+
+export class Header extends Component {
+    render() {
+        return (            
+            <nav className="navbar"> 
+                <p className="nav-title">Event Finder</p> 
+                <ul className="nav-links">                      
+                    <li className="nav-item">Event Search</li>  
+                    <li className="nav-item">Sign In</li>                    
+                </ul>  
+            </nav>
+        )
+    }
+}
+
+export default Header
---
+++ src/Components/SearchDialog/SearchDialog.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import './SearchDialog.css'
+
+export class SearchDialog extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            location: '',
+            event: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        const nam = e.target.name;
+        const val = e.target.value;
+        this.setState({[nam]: val});
+    }
+
+    handleSubmit(e) {
+        alert(`Location: ${this.state.location} \nEvent: ${this.state.event}`);
+        e.preventDefault();
+    }
+
+
+    render() {
+        return (                                               
+            <div className="search-dialog">   
+                <div className="search-title">             
+                    <h1>Find an event near you</h1>
+                </div>
+                <div className="search-options">
+                    <form onSubmit={this.handleSubmit}>                                       
+                        <label>
+                            Location:
+                            <input type="text" value={this.state.location} name="location" placeholder="Enter City" onChange={this.handleChange}></input>
+                        </label>
+                        <label>
+                            Event:
+                            <input type="text" value={this.state.event} name="event" placeholder="Event Name" onChange={this.handleChange}></input>
+                        </label>
+                        <input type="submit" value="Search Events"></input>
+                    </form>      
+                </div>          
+            </div>
+        )
+    }
+}
+
+export default SearchDialog
